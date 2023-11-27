e1d62fa449f00ec27bf13bb6f37a75190e45d57e
Joost <2006joost@gmail.com>
2023-11-27 09:55:58 +0100

    10.1

--- OOP/Zie_Scherper 10/Program.cs
+++ OOP/Zie_Scherper 10/Program.cs
@@ -1,11 +1,21 @@
 ﻿namespace Zie_Scherper_10
 {
-    internal class Program
+    public class Program
     {
+        int getal = 3;
+        int getal2 = 10;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int getal = 5;
+            VerhoogParameter(getal);
+            Console.WriteLine($"Na methode {getal}");
             Console.ReadLine();
+        }
+
+        static void VerhoogParameter(int a)
+        {
+            a++;
+            Console.WriteLine($"In methode {a}");
         }
     }
 }
