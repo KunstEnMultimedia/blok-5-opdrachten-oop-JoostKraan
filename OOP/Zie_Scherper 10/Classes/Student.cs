c510ec04c030a1687390e032ed3e13b6f6b8cf3e
Joost <2006joost@gmail.com>
2023-12-04 09:34:35 +0100

    10.1

---
+++ OOP/Zie_Scherper 10/Classes/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zie_Scherper_10.Classes
+{
+    public class Student
+    {
+        public string naam { get; set; } 
+
+        public Student(string naam)
+        {
+            this.naam = naam;
+            
+        }
+    
+    }
+}
--- OOP/Zie_Scherper 10/Program.cs
+++ OOP/Zie_Scherper 10/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Zie_Scherper_10
+﻿using System.Net.Http.Headers;
+using Zie_Scherper_10.Classes;
+
+namespace Zie_Scherper_10
 {
     public class Program
     {
@@ -6,11 +9,27 @@
         int getal2 = 10;
         static void Main(string[] args)
         {
+           
+
+        }
+
+        public static void opdracht10_01()
+        {
             int getal = 5;
             VerhoogParameter(getal);
             Console.WriteLine($"Na methode {getal}");
+
+            Student a = new Student("Abba");
+            Student b = new Student("Queen");
+            Console.WriteLine("naam Student a:" + a.naam);
+            Console.WriteLine("naam Student b:" + b.naam);
+            b = a;
+            Console.WriteLine("naam Student a:" + a.naam);
+            Console.WriteLine("naam Student b:" + b.naam);
             Console.ReadLine();
         }
+        
+       
 
         static void VerhoogParameter(int a)
         {
