dc394ea26fa92d57afa995bdc30b6f31300aefb4
Joost <113507203+JoostKraan@users.noreply.github.com>
2024-03-17 20:08:11 +0100

    12.02

--- OOP/ZieScherper_12/Program.cs
+++ OOP/ZieScherper_12/Program.cs
@@ -6,11 +6,11 @@ namespace ZieScherper_12
     {
         static void Main(string[] args)
         {
-           
+         
+
         }
         public static void Opdracht12_01()
         {
-            Student[] klas45 = { new Student(), new Student() };
             Student[] klas = new Student[5];
             for (int i = 0; i < klas.Length; i++)
             {
@@ -19,6 +19,21 @@ namespace ZieScherper_12
 
             klas[0].naam = "'Karel";
 
+        }
+
+        public static void Opdracht12_02()
+        {
+            Student student1 = new Student();
+            Student student2 = new Student();
+            List<Student> klasList = new List<Student>()
+            {
+                new Student() {naam = "Karel"},
+                new Student() {naam = "Bob"}
+            };
+            for (int i = 0; i < klasList.Count; i++)
+            {
+                Console.WriteLine(klasList[i].naam);
+            }
         }
     }
 }
